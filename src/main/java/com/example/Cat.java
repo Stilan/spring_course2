528b146906ebf6fa996f9a6c74738cac74cafb86
Stilan <40999153+Stilan@users.noreply.github.com>
2022-03-28 22:35:33 +0700

    17. Аннотация @Qualifier

--- src/main/java/com/example/Cat.java
+++ src/main/java/com/example/Cat.java
@@ -2,7 +2,7 @@ package com.example;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("catBean")
 public class Cat implements Pet {
 
     public Cat() {
--- src/main/java/com/example/Persson.java
+++ src/main/java/com/example/Persson.java
@@ -1,28 +1,31 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("perssonBean")
 public class Persson {
-    @Autowired
+//    @Autowired
+//    @Qualifier("catBean")
     private Pet pet;
     private String surname;
     private int age;
 
-//    @Autowired
-//    public Persson(Pet pet) {
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
-//    }
+    @Autowired
+    public Persson(@Qualifier("dog") Pet pet) {
+        System.out.println("Person bean is created");
+        this.pet = pet;
+    }
 
     public Persson() {
         System.out.println("Person bean is created");
     }
-// pet -> setPet
+//// pet -> setPet
 //    @Autowired
+//    @Qualifier("dog")
     public void setPet(Pet pet) {
-        System.out.println("Class Person: anyMethodName");
+        System.out.println("Class Person: setPet");
         this.pet = pet;
     }
 
