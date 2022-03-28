55b9b5c277204a0a793476db136d4aa458f968cf
Stilan <40999153+Stilan@users.noreply.github.com>
2022-03-28 17:51:14 +0700

    7. Dependency Injection с помощью сеттера

--- src/main/java/com/example/Persson.java
+++ src/main/java/com/example/Persson.java
@@ -3,8 +3,17 @@ package com.example;
 public class Persson {
     private Pet pet;
 
-    public Persson(Pet pet) {
+//    public Persson(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+    public Persson() {
         System.out.println("Person bean is created");
+    }
+// pet -> setPet
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
         this.pet = pet;
     }
 
--- src/main/java/com/example/Test3.java
+++ src/main/java/com/example/Test3.java
@@ -6,8 +6,8 @@ public class Test3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Pet pet = context.getBean("myPet", Pet.class);
-//        Pet pet = new Dog();
+//        Pet pet = context.getBean("myPet", Pet.class);
+////        Pet pet = new Dog();
 
         Persson persson = context.getBean("myPersson", Persson.class);
         persson.callYourPet();
