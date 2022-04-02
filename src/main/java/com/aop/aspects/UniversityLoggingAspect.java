d77711a14e7b784f6787ac084003d5ef00c2b3a7
Stilan <40999153+Stilan@users.noreply.github.com>
2022-04-02 21:38:57 +0700

    9. Advice тип @AfterReturning

---
+++ src/main/java/com/aop/aspects/UniversityLoggingAspect.java
@@ -0,0 +1,39 @@
+package com.aop.aspects;
+
+import com.aop.Student;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Aspect
+public class UniversityLoggingAspect {
+
+    @Before("execution(* getStudents())")
+    public void beforeGetStudentsLoggingAdvice() {
+        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение " +
+                "списка студентов перед метдом getStudents");
+    }
+
+    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
+    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
+        Student firstStudent = students.get(0);
+
+        String nameStudent = firstStudent.getNameSurname();
+        nameStudent = "Mr. " + nameStudent;
+        firstStudent.setNameSurname(nameStudent);
+
+        double avgGrade = firstStudent.getAvgGrade();
+        avgGrade = avgGrade + 1;
+        firstStudent.setAvgGrade(avgGrade);
+
+
+        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение " +
+                "списка студентов после работы метда getStudents");
+    }
+
+}
