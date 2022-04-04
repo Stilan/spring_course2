546cfe3e1c80d8774019b0d5103bff7d03478649
Stilan <40999153+Stilan@users.noreply.github.com>
2022-04-05 00:18:25 +0700

    12. Advice тип @Around. Знакомство

--- src/main/java/com/aop/UniLibrary.java
+++ src/main/java/com/aop/UniLibrary.java
@@ -11,9 +11,10 @@ public  class UniLibrary extends AbstractLibrary {
         System.out.println("------------------------------------");
     }
 
-     public void returnBook() {
+     public String returnBook() {
         System.out.println("Мы возвращаем книгу UniLibrary");
-         System.out.println("------------------------------------");
+        return "Война и мир";
+
     }
 
     public void getMagazine(){
