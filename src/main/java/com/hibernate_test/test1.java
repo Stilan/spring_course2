e74b861b7219435eb8906aa775334927a193e0dc
Stilan <40999153+Stilan@users.noreply.github.com>
2022-04-15 03:27:54 +0700

    5. Сохранение Java объектов в БД

---
+++ src/main/java/com/hibernate_test/test1.java
@@ -0,0 +1,29 @@
+package com.hibernate_test;
+
+import com.hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class test1 {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+
+            Employee emp = new Employee("Zaur", "Tregulov",
+                    "IT", 500);
+            session.beginTransaction();
+            session.save(emp);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+
+    }
+}
