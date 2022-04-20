5a5ff9abd4b25062b719cc832e14ef8a97500050
Stilan <40999153+Stilan@users.noreply.github.com>
2022-04-21 05:30:38 +0700

    12. Отношение Один-к-Одному (Uni-directional). Часть 2

--- src/main/java/com/hibernate_test_2/test1.java
+++ src/main/java/com/hibernate_test_2/test1.java
@@ -1,6 +1,8 @@
 package com.hibernate_test_2;
 
-import com.hibernate_test.entity.Employee;
+
+import com.hibernate_test_2.entity.Detail;
+import com.hibernate_test_2.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,14 +13,44 @@ public class test1 {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
                 .buildSessionFactory();
-
+        Session session = null;
         try {
-            Session session = factory.getCurrentSession();
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Zaur", "Tregulov"
+//                         , "IT", 500);
+//            Detail detail = new Detail("Baku", "123456789",
+//                    "zaurtregulov@gmail.com");
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Oleg", "Smirnov"
+//                    , "Sales", 700);
+//            Detail detail = new Detail("Moscow", "987654321",
+//                    "olejka@gmail.com");
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            Employee employee = session.get(Employee.class, 2);
+            session.delete(employee);
+
 
             session.getTransaction().commit();
 
         } finally {
+            session.close();
             factory.close();
         }
 
