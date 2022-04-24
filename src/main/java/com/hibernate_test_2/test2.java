d4a4fe6e7f743fbf345f07ce1d6df24c86d898a2
Stilan <40999153+Stilan@users.noreply.github.com>
2022-04-25 03:22:18 +0700

    13. Отношение Один-к-Одному (Bi-directional)

---
+++ src/main/java/com/hibernate_test_2/test2.java
@@ -0,0 +1,54 @@
+package com.hibernate_test_2;
+
+
+import com.hibernate_test_2.entity.Detail;
+import com.hibernate_test_2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class test2 {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+        Session session = null;
+        try {
+
+//            session = factory.getCurrentSession();
+//            Employee employee = new Employee("Nikolay", "Ivanov"
+//                    , "HR", 850);
+//            Detail detail = new Detail("New-York", "654123789",
+//                    "nikolay@gmail.com");
+//
+//            employee.setEmpDetail(detail);
+//            detail.setEmployee(employee);
+//            session.beginTransaction();
+//            session.save(detail);
+//
+//            session.getTransaction().commit();
+
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            Detail detail = session.get(Detail.class, 4);
+//            System.out.println(detail.getEmployee());
+//
+//            session.getTransaction().commit();
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Detail detail = session.get(Detail.class, 1);
+            detail.getEmployee().setEmpDetail(null);// розрушить связь
+            session.delete(detail);
+
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+}
